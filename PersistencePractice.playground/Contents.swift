2cc6227e0a61964b68e79ef9f73c12e407802ce6
iBamboola <41113234+iBamboola@users.noreply.github.com>
2021-04-21 11:43:03 +0300

    Done

--- PersistencePractice.playground/Contents.swift
+++ PersistencePractice.playground/Contents.swift
@@ -8,16 +8,30 @@ struct Note: Codable {
     let timestamp: Date
 }
 
-let newNote = Note(title: "Grocery run", text: "Pick up mayonnaise, mustard, lettuce, tomato, and pickles.", timestamp: Date())
-
-let propertyListEncoder = PropertyListEncoder()
-if let encodedNote = try? propertyListEncoder.encode(newNote) {
-    print(encodedNote)
-    let propertyListDecoder = PropertyListDecoder()
-    if let decodedNote = try? propertyListDecoder.decode(Note.self, from: encodedNote){
-        print(decodedNote)
-    }
-}
+let note1 = Note(title: "Note One", text: "This is a sample note.", timestamp: Date())
+let note2 = Note(title: "Note Two", text: "This is another sample note.", timestamp: Date())
+let note3 = Note(title: "Note Three", text: "This is yet another sample note.", timestamp: Date())
+let notes = [note1, note2, note3]
 
 let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
 print(documentDirectory)
+
+
+let archiveURL = documentDirectory.appendingPathComponent("notes_test").appendingPathExtension("plist")
+print(archiveURL)
+
+let propertyListEncoder = PropertyListEncoder()
+
+// encode and write
+let encodedNote = try? propertyListEncoder.encode(notes).write(to: archiveURL, options: .noFileProtection)
+
+// read and decode
+let propertyListDecoder = PropertyListDecoder()
+if let retrievedNoteData = try? Data(contentsOf: archiveURL) {
+    if let decodedNote = try? propertyListDecoder.decode(Array<Note>.self, from: retrievedNoteData){
+        for i in 0 ..< decodedNote.count {
+            print(decodedNote[i])
+        }
+    }
+}
+
